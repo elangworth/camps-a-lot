f126f8560102d31f61f345b399ffa0756c5c4c54
bwax <braxton.waxdeck@gmail.com>
2022-10-25 21:13:58 -0700

    finished date selector and temp form JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,75 @@
 
-var formEl = $('#camping-form');
-var tempInputEl = $('#temp-input');
-var dateInputEl = $('#datepicker');
-var submitButtonEl = $('#submit-button');
+var formEl = document.querySelector("#camping-form");
+var tempInputEl = document.querySelector("#temp-input");
+var dateInputEl = $("#datepicker");
+var submitButtonEl = document.querySelector("#submit-button");
 
+// debugger;
 //Datepicker widget
 $(function () {
-    $("#datepicker").datepicker({ minDate: 0, maxDate: '+10D' });
+    $("#datepicker").datepicker({ minDate: 0, maxDate: '+10D'
+    });
+   
 });
 
 
 
 //form submit console log inputs
 var handleFormSubmit = function (event) {
-    console.log("test")
-
     event.preventDefault();
 
     var tempInput = tempInputEl.value;
-    var dateInput = dateInputEl.value;
+    var dateInput = dateInputEl.val();
+
+    
+
+    console.log(tempInput);
+    console.log(dateInput);
 
    
    
 };
 
-//console.log(tempInputEl);
-
-formEl.on('submit', handleFormSubmit);
 
 
+formEl.addEventListener('submit', handleFormSubmit);
+
+
+//recreation.gov API
+// Curl
+// curl -X 'GET' \
+//   'https://ridb.recreation.gov/api/v1/campsites?limit=50&offset=0' \
+//   -H 'accept: application/json'
+//Base URL
+//https://ridb.recreation.gov/api/v1
+//API key
+//'apikey: 28893f32-2f30-41d5-88d5-c57a0066f8d7'
+//CORS https://rapidapi.com/pgarciamaurino/api/http-cors-proxy/details
+//recreation.gov does not work on CORs either :( need to change idea
+// use this -> https://rapidapi.com/apidojo/api/travel-advisor/
 
 
 
-function performSearch(search){
-    baseURL = 'https://maps.openweathermap.org/maps/2.0/weather/TA2/1/1/1?appid=203481f675fae76832d631c5ecaa6b09'; //I don't know what values to put for z, x and y so I just put 1 for now
-    fetch(baseURl)
-    .then (function(response) {
-        return response.json();
-    })
-    .then (function(data){
-        console.log(data);
-    })
-};
+// function performSearch(){
+//     var baseURL = 'https://ridb.recreation.gov/api/v1/campsites?limit=50&offset=0'; 
+//     fetch(baseURL,{headers: {apikey: '28893f32-2f30-41d5-88d5-c57a0066f8d7'}})
+//     .then (function(response) {
+//         return response.json();
+//     })
+//     .then (function(data){
+//         console.log(data);
+//     })
+// };
+// console.log(performSearch());
+
+// function performSearch(search){
+//     baseURL = 'https://maps.openweathermap.org/maps/2.0/weather/TA2/1/1/1?appid=203481f675fae76832d631c5ecaa6b09'; //I don't know what values to put for z, x and y so I just put 1 for now
+//     fetch(baseURl)
+//     .then (function(response) {
+//         return response.json();
+//     })
+//     .then (function(data){
+//         console.log(data);
+//     })
+// };
 
